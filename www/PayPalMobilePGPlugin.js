e0d9e033c655a4bdb2223a4c3e9ae5c64d6a4b06
Roman Punskyy <rpunskyy@paypal.com>
2014-03-04 11:29:15 -0800

    separate helper function from the wrapper
    * update plugin.xml to include asset for the helper
    * correctly define module

--- www/PayPalMobilePGPlugin.js
+++ www/PayPalMobilePGPlugin.js
@@ -3,113 +3,6 @@
 //
 
 /**
- * Convenience constructor. Returns a PayPalPayment with the specified amount, currency code, and short description.
- * @param amount The amount of the payment.
- * @param currencyCode The ISO 4217 currency for the payment.
- * @param shortDescription A short descripton of the payment.
- * @param intent Sale for an immediate payment or Auth
- *                 for payment authorization only, to be captured separately at a later time.
- */
-function PayPalPayment(amount, currency, shortDescription, intent) {
-  this.amount = amount;
-  this.currency = currency;
-  this.shortDescription = shortDescription;
-  this.intent = intent;
-}
-
-/**
- * The PayPalPaymentDetails class defines optional amount details.
- * @see https://developer.paypal.com/webapps/developer/docs/api/#details-object for more details.
- */
-function PayPalPaymentDetails(shipping, subtotal, tax) {
-  this.shipping = shipping;
-  this.subtotal = subtotal;
-  this.tax = tax;
-}
-
-
-/**
- * You use a getPayPalConfiguration object to configure many aspects of how the SDK behaves.
- * see defaults for options available
- */
-function PayPalConfiguration(options) {
-
-  defaults = {
-      /// Will be overridden by email used in most recent PayPal login.
-      defaultUserEmail : null,
-      /// Will be overridden by phone country code used in most recent PayPal login
-      defaultUserPhoneCountryCode : null,
-      /// Will be overridden by phone number used in most recent PayPal login.
-      /// @note If you set defaultUserPhoneNumber, be sure to also set defaultUserPhoneCountryCode.
-      defaultUserPhoneNumber : null,
-      /// Your company name, as it should be displayed to the user
-      /// when requesting consent via a PayPalFuturePaymentViewController.
-      merchantName : null,
-      /// URL of your company's privacy policy, which will be offered to the user
-      /// when requesting consent via a PayPalFuturePaymentViewController.
-      merchantPrivacyPolicyURL: null,
-      /// URL of your company's user agreement, which will be offered to the user
-      /// when requesting consent via a PayPalFuturePaymentViewController.
-      merchantUserAgreementURL: null,
-      /// If set to NO, the SDK will only support paying with PayPal, not with credit cards.
-      /// This applies only to single payments (via PayPalPaymentViewController).
-      /// Future payments (via PayPalFuturePaymentViewController) always use PayPal.
-      /// Defaults to YES
-      acceptCreditCards: true,
-      /// If set to YES, then if the user pays via their PayPal account,
-      /// the SDK will remember the user's PayPal username or phone number;
-      /// if the user pays via their credit card, then the SDK will remember
-      /// the PayPal Vault token representing the user's credit card.
-      ///
-      /// If set to NO, then any previously-remembered username, phone number, or
-      /// credit card token will be erased, and subsequent payment information will
-      /// not be remembered.
-      ///
-      /// Defaults to YES.
-      rememberUser: true,
-      /// If not set, or if set to nil, defaults to the device's current language setting.
-      ///
-      /// Can be specified as a language code ("en", "fr", "zh-Hans", etc.) or as a locale ("en_AU", "fr_FR", "zh-Hant_HK", etc.).
-      /// If the library does not contain localized strings for a specified locale, then will fall back to the language. E.g., "es_CO" -> "es".
-      /// If the library does not contain localized strings for a specified language, then will fall back to American English.
-      ///
-      /// If you specify only a language code, and that code matches the device's currently preferred language,
-      /// then the library will attempt to use the device's current region as well.
-      /// E.g., specifying "en" on a device set to "English" and "United Kingdom" will result in "en_GB".
-      ///
-      /// These localizations are currently included:
-      /// da,de,en,en_AU,en_GB,en_SV,es,es_MX,fr,he,it,ja,ko,nb,nl,pl,pt,pt_BR,ru,sv,tr,zh-Hans,zh-Hant_HK,zh-Hant_TW.
-      languageOrLocale: null,
-      /// Normally, the SDK blurs the screen when the app is backgrounded,
-      /// to obscure credit card or PayPal account details in the iOS-saved screenshot.
-      /// If your app already does its own blurring upon backgrounding, you might choose to disable this.
-      /// Defaults to NO.
-      disableBlurWhenBackgrounding : false,
-      /// Sandbox credentials can be difficult to type on a mobile device. Setting this flag to YES will
-      /// cause the sandboxUserPassword and sandboxUserPin to always be pre-populated into login fields.
-      ///
-      /// This setting will have no effect if the operation mode is production.
-      forceDefaultsInSandbox : false,
-      /// Password to use for sandbox if 'forceDefaultsInSandbox' is set.
-      sandboxUserPassword: null,
-      /// PIN to use for sandbox if 'forceDefaultsInSandbox' is set.
-      sandboxUserPin: null
-
-  };
-
-  if(!options || typeof options !== "object") {
-      return defaults;
-  }
-
-  for(var i in defaults) {
-        defaults[i] = options[i] || defaults[i];
-  }
-
-  return defaults;
-}
-
-
-/**
  * This class exposes the PayPal iOS SDK functionality to javascript.
  *
  * @constructor
@@ -117,17 +10,23 @@ function PayPalConfiguration(options) {
 function PayPalMobile() {}
 
 
+PayPalMobile.prototype.environment = {
+  SANDBOX: "PayPalEnvironmentSandbox",
+  PRODUCTION: "PayPalEnvironmentProduction",
+  NONETWORK: "PayPalEnvironmentNoNetwork"
+};
+
 /**
  * Retrieve the version of the PayPal iOS SDK library. Useful when contacting support.
  *
- * @parameter callback: a callback function accepting a string
+ * @parameter completionCallback: a callback function accepting a string
  */
-PayPalMobile.prototype.version = function(callback) {
+PayPalMobile.prototype.version = function(completionCallback) {
   var failureCallback = function() {
     console.log("Could not retrieve PayPal library version");
   };
 
-  cordova.exec(callback, failureCallback, "PayPalMobile", "version", []);
+  cordova.exec(completionCallback, failureCallback, "PayPalMobile", "version", []);
 };
 
 
@@ -140,12 +39,12 @@ PayPalMobile.prototype.version = function(callback) {
  *  sandbox : @"my-client-id-for-Sandbox"
  *  }
  */
-PayPalMobile.prototype.initializeWithClientIdsForEnvironments = function(clientIdsForEnvironments) {
+PayPalMobile.prototype.initializeWithClientIdsForEnvironments = function(clientIdsForEnvironments, completionCallback) {
   var failureCallback = function(error) {
     console.log(error);
   };
 
-  cordova.exec(null, failureCallback, "PayPalMobile", "initializeWithClientIdsForEnvironments", [clientIdsForEnvironments]);
+  cordova.exec(completionCallback, failureCallback, "PayPalMobile", "initializeWithClientIdsForEnvironments", [clientIdsForEnvironments]);
 };
 
 /**
@@ -156,12 +55,12 @@ PayPalMobile.prototype.initializeWithClientIdsForEnvironments = function(clientI
  *
  * @parameter environment: available options are "PayPalEnvironmentNoNetwork", "PayPalEnvironmentProduction" and "PayPalEnvironmentSandbox"
  */
-PayPalMobile.prototype.preconnectWithEnvironment = function(environment) {
+PayPalMobile.prototype.preconnectWithEnvironment = function(environment, completionCallback) {
   var failureCallback = function(error) {
     console.log(error);
   };
 
-  cordova.exec(null, failureCallback, "PayPalMobile", "preconnectWithEnvironment", [environment]);
+  cordova.exec(completionCallback, failureCallback, "PayPalMobile", "preconnectWithEnvironment", [environment]);
 };
 
 
@@ -176,8 +75,8 @@ PayPalMobile.prototype.preconnectWithEnvironment = function(environment) {
  * @parameter completionCallback: a callback function accepting a js object, called when the user has completed payment
  * @parameter cancelCallback: a callback function accepting a reason string, called when the user cancels the payment
  */
-PayPalMobile.prototype.presentSinglePaymentUI = function(payment, paymentDetails, configuration, completionCallback, cancelCallback) {
-  cordova.exec(completionCallback, cancelCallback, "PayPalMobile", "presentSinglePaymentUI", [payment, paymentDetails, configuration]);
+PayPalMobile.prototype.presentSinglePaymentUI = function(payment, configuration, completionCallback, cancelCallback) {
+  cordova.exec(completionCallback, cancelCallback, "PayPalMobile", "presentSinglePaymentUI", [payment, configuration]);
 };
 
 
@@ -214,4 +113,18 @@ PayPalMobile.prototype.presentFuturePaymentUI = function(configuration, completi
 /**
  * Plugin setup boilerplate.
  */
-module.exports = PayPalMobile;
+
+module.exports = {
+  version : function(completionCallback) {
+    var failureCallback = function() {
+      console.log("Could not retrieve PayPal library version");
+    };
+
+    cordova.exec(completionCallback, failureCallback, "PayPalMobile", "version", []);
+  }
+};
+
+// module.exports.PayPalMobile = new PayPalMobile();
+// module.exports.PayPalPaymentDetails = new PayPalPaymentDetails();
+// module.exports.PayPalPayment = new PayPalPayment();
+// module.exports.PayPalConfiguration = new PayPalConfiguration();
