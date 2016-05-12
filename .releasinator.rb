c44c9d87b3e01c467ee4110401010ccefb345726
Bhavani Bheemanadham <bbheemanadham@paypal.com>
2016-05-12 11:54:12 -0500

    updated releasinator

--- .releasinator.rb
+++ .releasinator.rb
@@ -3,19 +3,19 @@ configatron.product_name = "PayPal-Cordova-Plugin"
 
 # List of items to confirm from the person releasing.  Required, but empty list is ok.
 configatron.prerelease_checklist_items = [  
-  "Run the IOS Tests.",
+  "Run the iOS Tests.",
   "Run the Android Tests.",
   "Sanity check the master branch."
 ]
 
 def validate_version_match()
-  if plugin_version != @current_release.version
+  if plugin_version() != @current_release.version
     Printer.fail("Plugin.xml version #{plugin_version} does not match changelog version #{@current_release.version}.")
     abort()
   end
   Printer.success("Plugin.xml version #{plugin_version} matches latest changelog version.")
 
-  if package_version != @current_release.version
+  if package_version() != @current_release.version
       Printer.fail("Package.json version #{package_version} does not match changelog version #{@current_release.version}.")
       abort()
     end
@@ -26,7 +26,7 @@ configatron.custom_validation_methods = [
   method(:validate_version_match)
 ]
 
-# there are no separate build steps for PayPal-Cordova-Plugin , so it is just empty method
+# there are no separate build steps for PayPal-Cordova-Plugin, so it is just empty method
 def build_method
 end
 
@@ -42,7 +42,7 @@ configatron.publish_to_package_manager_method = method(:publish_to_package_manag
 
 
 def wait_for_package_manager(version)
-  CommandProcessor.wait_for("wget -U \"non-empty-user-agent\" -qO- https://www.npmjs.com/package/com.paypal.cordova.mobilesdk |grep #{@current_release.version} | cat")
+  CommandProcessor.wait_for("wget -U \"non-empty-user-agent\" -qO- https://www.npmjs.com/package/com.paypal.cordova.mobilesdk | grep #{@current_release.version} | cat")
 end
 
 # The method that waits for the package manager to be done.  Required
@@ -53,22 +53,21 @@ configatron.release_to_github = true
 
 
 def plugin_version()
-  f=File.new("plugin.xml", 'r')
-  f.each do |line|
-     if line.match (/version=\"\d*\.\d*\.\d*\"/)
-       return line.strip.split('=')[1].strip.split('"')[1]
+  f=File.open("plugin.xml", 'r') do |f|
+    f.each_line do |line|
+      if line.match (/version=\"\d*\.\d*\.\d*\"/)
+        return line.strip.split('=')[1].strip.split('"')[1]
+      end
     end
   end
-  f.close
 end
 
 def package_version()
-f=File.new("Package.json", 'r')
-  f.each do |line|
-    if line.match (/\"version\": \"\d*\.\d*\.\d*\"/)
-      return line.strip.split(':')[1].strip.split('"')[1]
+  f=File.open("Package.json", 'r') do |f|
+    f.each_line do |line|
+      if line.match (/\"version\": \"\d*\.\d*\.\d*\"/)
+        return line.strip.split(':')[1].strip.split('"')[1]
+      end
     end
   end
-  f.close
 end
-
